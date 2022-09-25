827abe4edbc1993ecbdb5182811e57fbed301b05
jackbui05 <jackbui05@hotmail.com>
2022-09-24 20:15:44 -0700

    test phase

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,13 +1,33 @@
 package main.java;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Queue<Object> queueClassroom = new LinkedList<>(); 
+        
 
 
+        //System.out.println("Hello World");
+
+        multipleChoiceQuestion question = new multipleChoiceQuestion(); 
+
+        question.addAnswer("A");
+        String [] answers = question.getAnswers();
+        for(int i = 0; i < answers.length; i++){
+            System.out.println(answers[i]);
+        }
+        //System.out.println(question.getAnswers());
+        
         Student student = new Student();
-        student.setName("James");
-        student.setStudentID(12345);
-        System.out.println(student.Name());
+        System.out.println(student.getId());
+        queueClassroom.add(student);
+        //System.out.println(queueClassroom.peek());
+        //Object test = queueClassroom.peek(); 
+        
+        //System.out.print(test);
+        //System.out.println(queueClassroom.isEmpty());
+
     }
 }
