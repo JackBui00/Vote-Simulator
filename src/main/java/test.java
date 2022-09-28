26d148a6195578ea745e4c00ae3e3c96418487ab
jackbui05 <jackbui05@hotmail.com>
2022-09-28 14:31:11 -0700

    comment

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -7,20 +7,19 @@ class Main{
     public static void main(String[] args) { 
         Random rand = new Random(); 
 
-        System.out.println("testing");
-
         int students = rand.nextInt(25) + 1; 
 
         int questions = rand.nextInt(5) + 1;
 
         int multiQuestions = rand.nextInt(5) + 1;
 
-        
+        //loop amount of random questions 
         for (int i = 0; i < questions; i++){
             SimulationDriver run = new SimulationDriver(students, questions,multiQuestions); 
             int[] xsize = run.createAnswerQuestion(questions);
             run.displayQuestionAnswer(xsize, students);
         }
+        //loop amount of random questions 
         for (int i = 0; i < multiQuestions; i++){
             SimulationDriver run = new SimulationDriver(students, questions,multiQuestions); 
             int[] xsize = run.createAnswerMultiQuestion(questions);
